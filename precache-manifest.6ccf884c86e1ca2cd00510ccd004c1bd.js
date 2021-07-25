c5daa515183e9fa26801982dedddc6a64871b579
Skudilo <skudilo@gmail.com>
2021-07-25 17:38:58 +0300

    Updates

---
+++ precache-manifest.6ccf884c86e1ca2cd00510ccd004c1bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcda1b65e64c59a121eec52418217e7",
+    "url": "/GitHub-search/index.html"
+  },
+  {
+    "revision": "1e76ada89e04ca3dcbde",
+    "url": "/GitHub-search/static/css/main.ceb9dfde.chunk.css"
+  },
+  {
+    "revision": "907f841ee51a4997bfcf",
+    "url": "/GitHub-search/static/js/2.15d2e38a.chunk.js"
+  },
+  {
+    "revision": "1e76ada89e04ca3dcbde",
+    "url": "/GitHub-search/static/js/main.f7457b12.chunk.js"
+  },
+  {
+    "revision": "e10bce6b6f07197c59b6",
+    "url": "/GitHub-search/static/js/runtime~main.bf78161d.js"
+  }
+]);
